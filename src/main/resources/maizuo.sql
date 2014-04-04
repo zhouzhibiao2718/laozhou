7cdfd4cb75dcd2fa3140989c379d099d14981469
Table <biaoqier@qq.com>
2014-04-04 12:43:46 +0800

    Signed-off-by: Table <biaoqier@qq.com>

--- src/main/resources/maizuo.sql
+++ src/main/resources/maizuo.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50612
 Source Host           : localhost:3306
-Source Database       : crm
+Source Database       : maizuo
 
 Target Server Type    : MYSQL
 Target Server Version : 50612
